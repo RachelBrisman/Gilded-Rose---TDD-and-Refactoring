4dd152b762bf93219e3ec866e506e49cf633b136
RachelBrisman <rachaelibrisman@gmail.com>
2023-02-16 16:05:11 +0200

    minor refactoring tweak, can help future

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -5,13 +5,15 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def increase_quality(self, item):
-        if item.quality < 50:
-            item.quality += 1
+    def increase_quality(self, item, times):
+        for x in range(times):
+            if item.quality < 50:
+                item.quality += 1
     
-    def decrease_quality(self, item):
-        if item.quality > 0:
-            item.quality -= 1
+    def decrease_quality(self, item, times):
+        for x in range(times):
+            if item.quality > 0:
+                item.quality -= 1
 
     def update_quality(self):
         for item in self.items:
@@ -22,18 +24,17 @@ class GildedRose(object):
 
             # UPDATE QUALITY
             if item.name == "Aged Brie":
-                self.increase_quality(item)
+                self.increase_quality(item, 1)
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                self.increase_quality(item)
+                self.increase_quality(item, 1)
                 if item.sell_in < 11:
-                    self.increase_quality(item)
+                    self.increase_quality(item, 1)
                 if item.sell_in < 6:
-                    self.increase_quality(item)
+                    self.increase_quality(item, 1)
             elif item.name == "Conjured":
-                self.decrease_quality(item)
-                self.decrease_quality(item)
+                self.decrease_quality(item, 2)
             else: # regular item
-                self.decrease_quality(item)
+                self.decrease_quality(item, 1)
                 
             # DECREASE SELL IN
             item.sell_in -= 1
@@ -41,14 +42,13 @@ class GildedRose(object):
             # FOR EXPIRED ITEMS, continue to update the quality    
             if item.sell_in < 0:
                 if item.name == "Aged Brie":
-                    self.increase_quality(item)
+                    self.increase_quality(item, 1)
                 elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                     item.quality = 0
                 elif item.name == "Conjured":
-                    self.decrease_quality(item)
-                    self.decrease_quality(item)
+                    self.decrease_quality(item, 2)
                 else: # regular item
-                    self.decrease_quality(item)
+                    self.decrease_quality(item, 1)
 
 class Item:
     def __init__(self, name, sell_in, quality):
